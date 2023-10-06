5e4d8629505f5619d276278fd3e568adcc2a86de
Dave Sánchez <Brahyams@gmail.com>
2023-10-06 11:13:29 -0500

    feat: LiveData has been replaced by StateFlow

--- MovieList/app/src/main/java/com/example/movielist/MainActivity.kt
+++ MovieList/app/src/main/java/com/example/movielist/MainActivity.kt
@@ -3,8 +3,9 @@ package com.example.movielist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
@@ -14,23 +15,28 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import com.example.movielist.ui.theme.MovieListTheme
 
-//MVVM Model view - view model
 // 4e44bcbe12d9628a90e27203233519f4
 
 class MainActivity : ComponentActivity() {
@@ -43,7 +49,7 @@ class MainActivity : ComponentActivity() {
             MovieListTheme {
 
                 val viewModel: MainViewModel = viewModel()
-                val state by viewModel.state.observeAsState(MainViewModel.UiState())
+                val state by viewModel.state.collectAsState()
 
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -53,32 +59,31 @@ class MainActivity : ComponentActivity() {
                     Scaffold(
                         topBar = { TopAppBar(title = { Text("Movie List") }) }
                     ) { padding ->
-                        LazyVerticalGrid(
-                            columns = GridCells.Adaptive(120.dp),
-                            modifier = Modifier.padding(padding),
-                            horizontalArrangement = Arrangement.spacedBy(4.dp),
-                            verticalArrangement = Arrangement.spacedBy(4.dp),
-                            contentPadding = PaddingValues(4.dp)
-                        ) {
-                            items(state.movies) { movie ->
-                                Column {
-                                    AsyncImage(
-                                        model = "https://image.tmdb.org/t/p/w185/${movie.poster_path}",
-                                        contentDescription = movie.title,
-                                        modifier = Modifier
-                                            .fillMaxWidth()
-                                            .aspectRatio(2 / 3f)
-                                    )
-                                    Text(
-                                        text = movie.title,
-                                        modifier = Modifier.padding(8.dp),
-                                        maxLines = 2
-                                    )
+
+                        if (state.loading) {
+                            Box(
+                                modifier = Modifier.fillMaxSize(),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                CircularProgressIndicator()
+                            }
+                        }
+                        if (state.movies.isNotEmpty()) {
+                            LazyVerticalGrid(
+                                columns = GridCells.Adaptive(120.dp),
+                                modifier = Modifier.padding(padding),
+                                horizontalArrangement = Arrangement.spacedBy(4.dp),
+                                verticalArrangement = Arrangement.spacedBy(4.dp),
+                                contentPadding = PaddingValues(4.dp)
+                            ) {
+                                items(state.movies) { movie ->
+                                    MovieItem(
+                                        movie = movie,
+                                        onClick = { viewModel.onMovieClick(movie) })
                                 }
                             }
                         }
                     }
-
                 }
             }
         }
@@ -86,17 +91,42 @@ class MainActivity : ComponentActivity() {
 
 
     @Composable
-    fun Greeting(name: String, modifier: Modifier = Modifier) {
-        Text(
-            text = "Hello $name!", modifier = modifier
-        )
+    fun MovieItem(movie: ServerMovie, onClick: () -> Unit) {
+        Column(
+            modifier = Modifier.clickable(onClick = onClick)
+        ) {
+            Box {
+                AsyncImage(
+                    model = "https://image.tmdb.org/t/p/w185/${movie.poster_path}",
+                    contentDescription = movie.title,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(2 / 3f)
+                )
+                if (movie.favorite) {
+                    Icon(
+                        imageVector = Icons.Default.Favorite,
+                        contentDescription = "Icono favorito",
+                        modifier = Modifier
+                            .align(Alignment.TopEnd)
+                            .padding(4.dp),
+                        tint = Color.White
+                    )
+                }
+            }
+
+            Text(
+                text = movie.title,
+                modifier = Modifier.padding(8.dp),
+                maxLines = 2
+            )
+        }
     }
 
     @Preview(showBackground = true)
     @Composable
     fun GreetingPreview() {
         MovieListTheme {
-            Greeting("Android")
         }
     }
 }
--- MovieList/app/src/main/java/com/example/movielist/MainViewModel.kt
+++ MovieList/app/src/main/java/com/example/movielist/MainViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -15,22 +16,35 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class MainViewModel : ViewModel() {
 
-    private val _state = MutableLiveData(UiState())
-    val state: LiveData<UiState> = _state
+    private val _state = MutableStateFlow(UiState())
+    val state: StateFlow<UiState> = _state
 
     init {
         viewModelScope.launch {
-            _state.value = UiState(Retrofit.Builder().baseUrl("https://api.themoviedb.org/3/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-                .create(MovieService::class.java)
-                .getMovies()
-                .results)
+            _state.value = UiState(loading = true)
+            delay(1000)
+            _state.value = UiState(
+                movies = Retrofit.Builder().baseUrl("https://api.themoviedb.org/3/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+                    .create(MovieService::class.java)
+                    .getMovies()
+                    .results
+            )
         }
+    }
+
+    fun onMovieClick(movie: ServerMovie) {
+        val movies = _state.value.movies.toMutableList()
+
+        movies.replaceAll { if (it.id == movie.id) movie.copy(favorite = !movie.favorite) else it }
+
+        _state.value = _state.value.copy(movies = movies)
 
     }
 
     data class UiState(
+        val loading: Boolean = false,
         val movies: List<ServerMovie> = emptyList()
     )
 }
